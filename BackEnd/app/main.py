d5bc19e693c0a4cf71e50d055279683375475761
FSFCR16 <fajarfsabo161@gmail.com>
2026-03-07 13:10:24 -0600

    TEST CORS

--- BackEnd/app/main.py
+++ BackEnd/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 ### CORS
 origins = [URLFRONT, "http://localhost:3000"]
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, 
+app.add_middleware(CORSMiddleware, allow_origins=[*], allow_credentials=True, 
                    allow_methods=["*"], allow_headers=["*"])
 
 ### RUTAS 
